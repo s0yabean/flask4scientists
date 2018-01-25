15364f85096e3383d825e9b8c70ac1587c0b8caf
s0yabean <tszhin07@hotmail.com>
2018-01-25 14:04:41 -0800

    added wtforms package

--- controller_flask.py
+++ controller_flask.py
@@ -63,7 +63,10 @@ def index():
         return render_template("view_input.html", form = form)
     
 if __name__ == '__main__':
-    app.run(debug = True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=True)
+    
 
 
 
